ee9cdaa607c9ce76e7342968daf1bc5cdc4cb49c
ikerrios <1925184@alu.murciaeduca.es>
2025-10-27 13:47:34 +0100

    finalizado

--- view/productList.php
+++ view/productList.php
@@ -3,7 +3,7 @@
         <section>
             <?php foreach($product as $p){ ?>
                 <ul class="list-group">
-                    <li class="list-group-item"><b>Código</b>:<?php echo $p->getCod();?></li>
+                    <li class="list-group-item"><b>Código:</b>:<?php echo $p->getCod();?></li>
                     <li class="list-group-item"><b>Short Name:</b> <?= $p->getShort_name();?></li>
                     <li class="list-group-item"><b>PVP:</b> <?= $p->getPvp();?></li>
                     <li class="list-group-item"><b>Nombre:</b> <?= $p->getName();?></li>
